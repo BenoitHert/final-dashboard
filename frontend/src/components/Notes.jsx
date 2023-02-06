68199290a06883eef28f4c2f034f6c26421c0ac8
salted-brocco-lee <benoit.hert@hotmail.fr>
2023-02-06 09:23:57 +0100

    b

---
+++ frontend/src/components/Notes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+
+function Notes() {
+    const dispatch = useDispatch()
+  return (
+    <div>
+        <div>
+            
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Notes
